8cc86b622fbde8bb889adae6aed68344ce22e801
kenhigashi-wq <ken.higashi@ucas-edu.net>
2026-01-12 10:22:22 -0700

    hde

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -5,7 +5,48 @@
 #Function for Budget Allocator
 #Function for Sale price calculator
 
+def savings():
+    goal = float(input("What amount are you saving to: $"))
+    print("How often will you contribute:\n1. weekly\n2. monthly")
+    choice = int(input("choose 1 or 2: "))
+    contribution = float(input("How much are you contributing each time: $"))
+    if choice == 2:
+        months = goal/contribution
+        print(f"It will take {months} months to save up ${goal:.2f}")
+    elif choice == 1:
+        weeks = goal/contribution
+        print(f"It will take {weeks} weeks to save up ${goal:.2f}")
+    else:
+        print("Invalid option")
 
+def compound():
+    money = float(input("Starting Amount: $"))
+    rate = float(input("Interest Rate percent: "))
+    years = int(input("Years spent compounding: "))
+    total = money * ((1 + rate / 100) ** years)
+    print(f"At the end of {years} you will have a total of ${total:.2f}")
+
+
+
+def budget_allocator():
+    num_categories = int(input("How many budget categories do you have: "))
+
+    categories = []
+    for i in range(num_categories):
+        name = input(f"Category {i+1} name:")
+        categories.append(name)
+    
+    income = float(input("What is your monthly income: "))
+    percents = []
+    for i in range(num_categories):
+        percent = float(input(f"What percent is your {categories[i]}: "))
+        percents.append(percent)
+    def calculate_amount(index):
+        return income * (percents[index]/ 100)
+        
+    for i in range(num_categories):
+        amount = calculate_amount(i)
+        print(f"{categories[i]} is ${amount:.2f}")
 
 
 
@@ -38,7 +79,11 @@ def main():
     print("4. Sale Price Calculator")
     print("5. Tip Calculator")
     op = input("Enter:")
-    if op == "4":
+    if op == "1":
+        savings()
+    elif op == "2":
+        compound()
+    elif op == "4":
         sale()
     elif op == "5":
         tip()
---
+++ notes/lists.py
@@ -0,0 +1,16 @@
+#KH lists notes
+
+
+siblings = ["alex", "me", "sarah", "joh"]
+siblings[-1] = "DOW"
+siblings.append("Alex")
+print(siblings)
+#categoris, data storag, control randomiztation 
+
+#unpack tuple
+white = (255,255,255)
+print(white(0))
+r,g,b = white
+print(r)
+
+# Level info, start point info, preset data, anything you don't want changed!
