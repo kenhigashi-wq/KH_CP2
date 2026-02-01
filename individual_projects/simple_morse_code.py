ea69170148ff0af862e46b0c780cdc3bd0466ba5
kenny <ken.higashi@ucas-edu.net>
2026-02-01 18:24:44 +0000

    SImple morse code translator

---
+++ individual_projects/simple_morse_code.py
@@ -0,0 +1,62 @@
+#KH 2nd Simple morse code translator
+
+english = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+           "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+             "u", "v", "w", "x", "y", "z")
+
+morse = ( 
+".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+    "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+    "..-","...-",".--","-..-","-.--","--.."
+)
+
+#making english, into moress code function
+def eng_to_morse(text):
+    result = ""
+    for char in text.lower():
+        if char in english:
+            result += morse[english.index(char)] + " "
+        elif char == " ":
+            result += "/ "
+        else:
+            continue
+    return result.strip()
+
+def morse_to_eng(code):
+    result = ""
+    letters = code.strip().split()
+    for symbol in letters:
+        if symbol == "/":
+            result += " "
+            continue
+        if symbol in morse:
+            result += english[morse.index(symbol)]
+        else:
+            continue
+    return result
+
+#make the main function
+def main():
+    print("Welcome to translator for morse code")
+
+    while True:
+        print("1. Translate from morse code to english")
+        print("2. Translate from English to morse code")
+        print("3. Exit")
+
+        choice = input("Coose an option(1-3): ")
+        if choice == "1":
+            code = input("What is the code you want to ttranslate? ")
+            print("Your code says: ")
+            print(morse_to_eng(code))
+        elif choice == "2":
+            text = input("What is the message you need in morse code? ")
+            print("Your message says:")
+            print(eng_to_morse(text))
+        elif choice == "3":
+            print("bye")
+            break
+        else:
+            print("Invalid, try again")
+
+main()
