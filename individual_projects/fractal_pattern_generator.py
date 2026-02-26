7b27f24be6e1341d1a156e7ab454eb86bf480e73
kenhigashi-wq <ken.higashi@ucas-edu.net>
2026-02-26 10:32:56 -0700

    recur nots

---
+++ individual_projects/fractal_pattern_generator.py
@@ -0,0 +1,8 @@
+#KH fractal function
+
+def main():
+    print("Welcome to the Sierpinski Triangle Generator! This program creates a Sierpinski Triangle fractal using recursion.")
+    depth = input("Enter recursion depth (1-5): ")
+    tri_color = input("Enter triangle color (e.g., red, blue, green): ")
+    print("Generating Sierpinski Triangle...")
+
