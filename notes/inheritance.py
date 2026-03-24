117e8278a5d706e3db61fdbe6107b0bf962008c1
kenhigashi-wq <ken.higashi@ucas-edu.net>
2026-03-24 10:49:02 -0600

    Inheritance notes

--- notes/inheritance.py
+++ notes/inheritance.py
@@ -9,9 +9,20 @@ class Vehicle:
     def move(self):
         print("Vroooom!")
 
+#
 # Child
+#Compisition
+class Engine:
+    def __init__(self, model):
+        self.cylinders = model
+
+    def __str__(self):
+        return self.model
+
 class Car(Vehicle):
-    pass
+    def __init__(self, brand, model):
+        super().__init__(brand, model)
+        self.engine = Engine("v8")
 
 class Boat(Vehicle):
     def move(self):
@@ -29,3 +40,42 @@ for x in (car, boat, plane):
     print(x.brand)
     print(x.model)
     x.move()
+
+print(car.engine)
+
+#Aggregate Classes
+class Library:
+    def __init__(self, name, catalog = []):
+        self.name = name
+        self.catalog = catalog
+
+    def add_book(self, book):
+        self.catalog.append(book)
+
+    def remove_book(self, book):
+        if book in self.catalog:
+            self.catalog.pop(book)
+        else:
+            print("Invalid")
+
+    def view_catalog(self):
+        for book in self.catalog:
+            print(book)
+
+    
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+lib = Library("Provo Libary")
+
+lib.add_book(Book("The way of kings", "Brandon Sanderson"))
+lib.add_book(Book("Test_book", "Test_author"))
+lib.add_book(Book("The last Battle", "C.S Lewis"))
+
+lib.view_catalog()
+
