1802243e1a1717ae931b22e6d3bb0dc1b68f301a
kenhigashi-wq <ken.higashi@ucas-edu.net>
2026-04-07 10:48:24 -0600

    Quote of the day "Before you criticize someone, walk a mile in their shoes. That way, you'll be a mile from them, and you'll have their shoes

--- notes/gui.py
+++ notes/gui.py
@@ -16,11 +16,19 @@ root.count = 0
 
 def add():
     root.count += 1
+    lbl['text'] = str(root.count)
+
+def sub():
+    root.count -= 1
+    lbl['text'] = str(root.count)
 
 btn = tk.Button(root, text="ADD", command=add)
 btn.pack()
+btn2 = tk.Button(root, text="SUB", command=sub)
+btn2.pack()
 
 lbl = tk.Label(root, text= "0")
+lbl.pack()
 
 root.minsize(250, 250)
 root.maxsize(1500, 1500)
